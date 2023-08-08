797d13f4a76a67ae8c446dfec35bf495a87a2cef
Chamel Nadir Bouacha <lc_bouacha@esi.dz>
2023-08-08 22:44:31 +0100

    finished the color flipper project using dom js

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,11 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+let btn = document.getElementById("btn");
+let color = document.querySelector(".color");
+let body = document.querySelector("body");
+
+btn.addEventListener("click", ()=>{
+    let colorRandom = Math.floor(Math.random()*colors.length);
+body.style.backgroundColor=colors[colorRandom];
+color.textContent=colors[colorRandom];
+color.style.color=colors[colorRandom];
+})
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,28 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+let btn = document.getElementById("btn");
+let color = document.querySelector(".color");
+let body = document.querySelector("body");
+
+
+btn.addEventListener("click", ()=>{
+    let hexColor = "#";
+    for(let i=0; i<6; i++){
+        hexColor += hex[getRandomNumber(hex.length)];
+    }
+    body.style.backgroundColor=hexColor;
+    color.textContent=hexColor;
+    color.style.color=hexColor;
+})
+const getRandomNumber = (num)=>{
+    return Math.floor(Math.random()*num);
+}
+
+
+
+
+
+
+
+
+
+
