329044a898598cf6f8e2b1c0a0dc5b7edcae937a
Chamel Nadir Bouacha <lc_bouacha@esi.dz>
2023-08-10 01:28:12 +0100

    Made a simple question components using js

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,32 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
+questions.forEach(question => {
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener("click", () => {
+
+        question.classList.toggle('show-text');
+        for (let i=0 ; i<questions.length ; i++) { 
+            if (questions[i] !== question) {
+                questions[i].classList.remove('show-text');
+            }
+        }
+    } ) ; 
+});
+
+
+
+
 // traversing the dom
+/* const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(btn => {
+ btn.addEventListener('click', (e) => {
+    for (let i=0 ; i<btns.length ; i++) { 
+        if (btns[i] !== e.currentTarget) {
+            btns[i].parentElement.parentElement.classList.remove('show-text');
+        }
+    }
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle('show-text');});
+
+}); */
