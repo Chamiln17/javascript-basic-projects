aefecf88688c036414f69bf9953b9fc3655effad
Chamel Nadir Bouacha <lc_bouacha@esi.dz>
2023-08-13 18:33:53 +0100

    Added The countdown using JavaScript

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,49 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+const items = document.querySelectorAll(".deadline-format h4");
+const deadline = document.querySelector(".deadline");
+const giveaway = document.querySelector(".giveaway");
+
+let futureDate = new Date(2023,7,20,10,0,0);
+const year = futureDate.getFullYear();
+const month = futureDate.getMonth();
+const date = futureDate.getDate();
+const day = futureDate.getDay();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const secs = futureDate.getSeconds();
+
+let format = (item) => {
+  if(item < 10){
+    return item = `0${item}`;
+  }
+  return item;
+};
+giveaway.textContent = `giveaway ends on ${weekdays[day]}, ${date} ${months[month]} ${year} ${hours}:${minutes}am`;
+
+//future time in ms
+const futureTime = futureDate.getTime();
+ 
+const getRemainingTime = () => {
+  let today = new Date().getTime();
+  let t = futureTime - today;
+  
+  //values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMin = 60 * 1000;
+
+  //calculate all values
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay)/oneHour);
+  let mins = Math.floor((t % oneHour)/oneMin);
+  let secs = Math.floor((t % oneMin)/1000);
+
+  let values = [days,hours,mins,secs];
+
+  items.forEach((item,index) => {
+    item.innerHTML = format(values[index]);
+  });
+};
+let countDown = setInterval(getRemainingTime,1000);
